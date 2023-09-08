06fb4d4dd9640df1de2bd95dee087d6f270ddb4c
Josue1699 <alfredo.rodriguez@amdc.hn>
2023-09-08 15:11:24 -0600

    Api
    Regsitro,login,logout

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         //
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -22,5 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->registerPolicies();
+       
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::post('register', [PassportAuthControlle::class,'register']);
+Route::post('login', [PassportAuthControlle::class,'login']);
+Route::post('logout', [PassportAuthControlle::class,'logout']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('register', [PassportAuthControlle::class,'register']);
+    Route::post('logout', [PassportAuthControlle::class,'logout']);
+});
 
 Route::get('/', function () {
     return view('welcome');
